f2ac3241fa95a4e4290174d118d8271c4172feb0
NowHyeok <wpgur4694@naver.com>
2024-05-22 22:35:37 +0900

    first commit

---
+++ api/src/main/java/jh/YoutooMetoo/YoutooMetooApplication.java
@@ -0,0 +1,13 @@
+package jh.YoutooMetoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutooMetooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutooMetooApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/jh/YoutooMetoo/YoutooMetooApplicationTests.java
@@ -0,0 +1,13 @@
+package jh.YoutooMetoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutooMetooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
